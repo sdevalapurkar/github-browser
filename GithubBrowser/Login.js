a10430dea08ae6e12be556598e7bbd68f3482caf
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2017-05-24 15:51:52 -0500

    refactored auth service code from login.js into separate class

--- GithubBrowser/Login.js
+++ GithubBrowser/Login.js
@@ -90,38 +90,14 @@ class Login extends Component {
     //console.log('Signing in with username' + this.state.username);
     this.setState({showProgress: true});
 
-    var b = new buffer.Buffer(this.state.username + ':' + this.state.password);
-    var encodedAuth = b.toString('base64');
-
-    fetch('https://api.github.com/user', {
-      headers: {
-        'Authorization' : 'Basic ' + encodedAuth
-      }
-    })
-    .then((response) => {
-      if(response.status >= 200 && response.status < 300)
-      {
-        return response;
-      }
-
-      throw {
-        badCredentials: response.status == 401,
-        unknownError: response.status != 401
-      }
-    })
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      console.log(result);
-      this.setState({success: true});
-    })
-    .catch((err) => {
-      console.log('Sign in failed: ' + err);
-      this.setState(err)
-    })
-    .finally(() => {
-      this.setState({showProgress: false});
+    var authService = require('./AuthService');
+    authService.login({
+      username: this.state.username,
+      password: this.state.password,
+    }, (result) => {
+      this.setState(Object.assign({
+        showProgress: false
+      }, result));
     });
 
   }
